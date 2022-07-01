50e2cd8475efed0764d13e5d49674fcd142decd4
Felipe de Oliveira <Felipe.GameplayDev@outlook.com>
2022-07-01 11:10:26 -0300

    Update PlayerController.cs
    Quando o jogador pressiona as setas pra cima/pra baixo, o veículo se moverá para frente e para trás.
    Quando o jogador pressionar as setas esquerda/direita, o veículo gira.

--- PlayerController/Assets/Scripts/PlayerController.cs
+++ PlayerController/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private float speed;
+    [SerializeField] private float speed = 20.0f;
+    [SerializeField] private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
     void Start()
     {
         
@@ -14,6 +17,19 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
+        InputMoveRotateCar();
+    }
+
+    private void InputMoveRotateCar()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //Mover o carro para frente baseado no input vertical
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        //Rodar o carro baseado no input horizontal
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
