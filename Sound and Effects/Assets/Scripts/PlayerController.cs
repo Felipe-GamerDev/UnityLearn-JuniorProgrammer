f005617b94d83d45da9242671d5295c956692234
Felipe de Oliveira <Felipe.GameplayDev@outlook.com>
2022-07-08 10:43:47 -0300

    Alterações de movimentação
    O jogador começa a cena com uma animação de corrida rápida.
    Quando o jogador pula, há uma animação de salto.
    Quando o jogador cai, a animação de corrida é restaurada.

--- Sound and Effects/Assets/Scripts/PlayerController.cs
+++ Sound and Effects/Assets/Scripts/PlayerController.cs
@@ -9,22 +9,24 @@ public class PlayerController : MonoBehaviour
     public bool gameOver = false;
     private Rigidbody playerRigidbody;
     [SerializeField] private float jumpForce = 10;
-    
     private bool isOnGround = true;
+    private Animator playerAnim;
 
     private void Start() 
     {
         playerRigidbody = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+        playerAnim = GetComponent<Animator>();
     }
     
     private void FixedUpdate() 
     {
         //Input para o personagem pular
-        if(Input.GetKeyDown(inputKey) && isOnGround)
+        if(Input.GetKeyDown(inputKey) && isOnGround && !gameOver)
         {
             playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -37,9 +39,10 @@ public class PlayerController : MonoBehaviour
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
-            isOnGround = false;
             gameOver = true;
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
