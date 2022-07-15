ce1eef2e825fb35dc54fb03f1c7d6494466a2dc4
Felipe de Oliveira <Felipe.GameplayDev@outlook.com>
2022-07-15 17:37:10 -0300

    PowerUp and CountDown
    Quando o jogador coleta um powerup, um indicador visual aparece.
    Quando o jogador colide com um inimigo enquanto eles têm o powerup, o inimigo vai voando.
    Depois de um certo período de tempo, a capacidade de powerup e o indicador desaparecem.

--- Gameplay Mechanics/Assets/Scripts/PlayerController.cs
+++ Gameplay Mechanics/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
     private GameObject focalPoint;
     private Rigidbody playerRigibody;
     [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float powerupStrength = 20.0f;
 
     private void Start() 
     {
@@ -18,5 +21,40 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRigibody.AddForce(focalPoint.transform.forward * speed * forwardInput *Time.fixedDeltaTime);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.CompareTag("PowerUp"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+            powerupIndicator.gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(5);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(other.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (other.gameObject.transform.position - transform.position);
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            
+            //Personalizado
+            hasPowerup = false;
+            powerupIndicator.gameObject.SetActive(false);
+        }
     }
 }
