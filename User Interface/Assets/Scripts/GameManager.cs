56331ff495e84b83eadbbe0dccd083ec101b8da3
Felipe de Oliveira <Felipe.GameplayDev@outlook.com>
2022-07-20 12:21:08 -0300

    Difficulty
    Tela de título que permite ao usuário iniciar o jogo.
    Seleção de dificuldade que afeta a taxa de desova

--- User Interface/Assets/Scripts/GameManager.cs
+++ User Interface/Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    //Script para controlar o Spawn de objetos em cena. Aplicações na UI, como game over, pontuação e restart.
+    //Script para controlar o Spawn de objetos em cena. Aplicações na UI, como game over, pontuação, start game e reiniciar.
 
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
@@ -15,14 +15,13 @@ public class GameManager : MonoBehaviour
     public GameObject background;
     public bool isGameActive;
     public Button restartButton;
+    public GameObject titleScreen;
     private float spawnRate = 1.0f;
     private int score;
 
     private void Start() 
     {
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
-        UptadeScore(0);
+        
     }
 
     IEnumerator SpawnTarget()
@@ -54,5 +53,19 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(float difficulty)
+    {  
+        isGameActive = true;
+        titleScreen.gameObject.SetActive(false);
+        
+        spawnRate /= difficulty; //Essa aplicação controla a quantidade de objetos spawnados em cada dificuldade
+
+        scoreText.gameObject.SetActive(true); //Minha lógica pra deixar o UI melhor
+        background.SetActive(true); //Minha lógica pra deixar o UI melhor
+
+        StartCoroutine(SpawnTarget());
+        UptadeScore(0);
     }
 }
